a001b890c74573d9b8292abedaa4bcf1f3f092da
sinan can özer <sinancemozer@gmail.com>
2022-11-29 23:24:38 +0300

    lowercase update

--- src/StudentInformationSystem.java
+++ src/StudentInformationSystem.java
@@ -2,28 +2,28 @@ public class StudentInformationSystem {
     public static void main(String[] args) {
 
         Teacher physicsTeacher = new Teacher("Alec", "PHY", "356285448", 57);
-        Course Physics = new Course("Physics", "156", "PHY");
-        Physics.addTeacher(physicsTeacher);
+        Course physics = new Course("Physics", "156", "PHY");
+        physics.addTeacher(physicsTeacher);
 
         Teacher mathematicsTeacher = new Teacher("George", "MATH", "452885221", 43);
-        Course Mathematics = new Course("Mathematics", "211", "MATH");
-        Mathematics.addTeacher(mathematicsTeacher);
+        Course mathematics = new Course("Mathematics", "211", "MATH");
+        mathematics.addTeacher(mathematicsTeacher);
 
         Teacher biologyTeacher = new Teacher("Trevor", "BIO", "477152369", 39);
-        Course Biology = new Course("Biology", "305", "BIO");
-        Biology.addTeacher(biologyTeacher);
+        Course biology = new Course("biology", "305", "BIO");
+        biology.addTeacher(biologyTeacher);
 
-        Student firstStudent = new Student("Sinan", "551", "5", Physics, Mathematics, Biology);
+        Student firstStudent = new Student("Sinan", "551", "5", physics, mathematics, biology);
         firstStudent.addBulkExamNote(40,50, 60,70 ,80,90);
 
         firstStudent.isPass();
 
         System.out.println("=================================");
 
-        Student SecondStudent = new Student("Erdem", "287", "4", Physics, Mathematics, Biology);
-        SecondStudent.addBulkExamNote(74,85, 82,40, 93,60);
+        Student secondStudent = new Student("Erdem", "287", "4", physics, mathematics, biology);
+        secondStudent.addBulkExamNote(74,85, 82,40, 93,60);
 
-        SecondStudent.isPass();
+        secondStudent.isPass();
 
         System.out.println("==================================");
 
